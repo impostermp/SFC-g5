6e840761e619df861ae07ee1e93d08cd76b80a0b
Jonas <something@email.com>
2023-08-18 13:00:36 +0000

    test14

--- backend/push.sh
+++ backend/push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker_username_lower=$(echo "$docker_username" | tr '[:upper:]' '[:lower:]')
 echo "$docker_password" | docker login ghcr.io --username "$docker_username_lower" --password-stdin
-docker push "ghcr.io/$docker_username_lower/simple-fortune-cookie:1.0-${GIT_COMMIT::8}"
-docker push "ghcr.io/$docker_username_lower/simple-fortune-cookie:latest" &
+docker push "ghcr.io/$docker_username_lower/simple-fortune-cookie-backend:1.0-${GIT_COMMIT::8}"
+docker push "ghcr.io/$docker_username_lower/simple-fortune-cookie-backend:latest" &
 wait
--- frontend/build.sh
+++ frontend/build.sh
@@ -2,4 +2,4 @@
 docker_username_lower=$(echo "$docker_username" | tr '[:upper:]' '[:lower:]')
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="ghcr.io/${docker_username_lower}/"
-docker build -t "${DockerRepo}micronaut-app:latest" -t "${DockerRepo}micronaut-app:1.0-$Tag" 
+docker build -t "${DockerRepo}simple-fortune-cookie-frontend:latest" -t "${DockerRepo}simple-fortune-cookie-frontend:1.0-$Tag" 
