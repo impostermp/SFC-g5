d5d5753c260f06b536f52723e4c50f4ea73571d9
Jonas <something@email.com>
2023-08-18 13:58:19 +0000

    test20

--- frontend/build.sh
+++ frontend/build.sh
@@ -2,4 +2,5 @@
 docker_username_lower=$(echo "$docker_username" | tr '[:upper:]' '[:lower:]')
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="ghcr.io/${docker_username_lower}/"
+cd frontend
 docker build -t "${DockerRepo}simple-fortune-cookie-frontend:latest" -t "${DockerRepo}simple-fortune-cookie-frontend:1.0-$Tag" .
